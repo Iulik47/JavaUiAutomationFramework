c1f0464185058656e6da6d7d479163741e9f6bf2
Iulik47 <iulikk2013@gmail.com>
2023-06-15 23:38:45 +0300

    Implemented the FakeDataManager and created the first scenario

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,29 +1,74 @@
 package com.opencart;
 
+import com.opencart.managers.DataFakerManager;
 import com.opencart.managers.DriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 public class TestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Define a driver object that will be used for future actions.
-       WebDriver driver = DriverManager.getInstance().getDriver();
+        WebDriver driver = DriverManager.getInstance().getDriver();
 
-       driver.get("https://999.md/ru/");
+        driver.get("https://unsplash.com/s/photos/canada");
 
-       String currentWindowName = driver.getWindowHandle();
+        String currentWindowName = driver.getWindowHandle();
 
-       driver.switchTo().newWindow(WindowType.TAB);
+        driver.switchTo().newWindow(WindowType.TAB);
 
-       driver.get("https://mvnrepository.com/search?q=selenium+api");
+        driver.get("https://andreisecuqa.host/");
 
-       driver.close();
+        WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        accountIcon.click();
 
-       driver.switchTo().window(currentWindowName);
+        WebElement registerBtn = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+        registerBtn.click();
 
-       driver.get("https://unsplash.com/s/photos/canada");
+        String firstName = DataFakerManager.getRandomName();
+        System.out.println("The generated first name is: " + firstName);
 
-       driver.quit();
+        String lastName = DataFakerManager.getRandomName();
+        System.out.println("The generated last name is: " + lastName);
+
+        String email = DataFakerManager.getRandomEmail("bazalt", "@mug.dev");
+        System.out.println("The generated email is: " + email);
+
+        String password = DataFakerManager.getRandomPassword(21, 24);
+        System.out.println("The generated password is: " + password);
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys(firstName);
+
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys(lastName);
+
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        emailInput.sendKeys(email);
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys(password);
+
+        WebElement privacyToggle = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+        Thread.sleep(500);
+        privacyToggle.click();
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+        continueButton.click();
+
+        Thread.sleep(5000);
+
+        System.out.println(driver.getCurrentUrl());
+
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+
+        driver.get("https://andreisecuqa.host/");
+
+        driver.quit();
+
         System.out.println("The execution was finished");
 
     }
